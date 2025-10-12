e55eb0a4996de97dbb4c0079f59782c55e71ce79
Musharaf Altaf <musharafdar10@Gmail.com>
2025-10-12 16:25:29 +0530

    little edit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,3 +1,29 @@
+// class ApiError extends Error{
+//     constructor(
+//         statusCode,
+//         message = "something went wrong",
+//         Error = [],
+//         stack = " " 
+        
+//     ){ 
+//         super(message)
+//         this.statusCode = statusCode
+//         this.data = null
+//         this.message = message
+//         this.success = false;
+//         this.errors = errors
+
+//         if (stack) {
+//             this.stack = stack
+//         } else {
+//             Error.captureStackTrace(this,this.constructor)
+//         }
+//     }
+// }
+
+// export {ApiError}
+ 
+
 class ApiError extends Error{
     constructor(
         statusCode,
